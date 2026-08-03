0e8c9934375a4bb2e59e7fa136951ef98e72bfe4
G07\kasem <k>
2026-08-03 13:54:50 +0700

    change pdf

--- indexing.py
+++ indexing.py
@@ -111,5 +111,5 @@ def index_pdf(pdf_path: str):
 
 
 if __name__ == "__main__":
-    pdf_file = os.getenv("PDF_FILE", "ai-booklet.pdf")
+    pdf_file = os.getenv("PDF_FILE", "condition-loan.pdf")
     index_pdf(pdf_file)
